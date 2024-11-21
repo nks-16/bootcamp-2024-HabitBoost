5ee0c91faa4e960057ba44d0f4485e2e452209a4
Sumedha76 <sumedhahiremath@gmail.com>
2024-11-21 16:02:58 +0530

    Update Signup files

--- client/src/login.tsx
+++ client/src/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import'./index.css';
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState('');
